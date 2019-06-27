51901e02b0f987f55ff3b4f24c6fcd48b91b6293
Hiroshi Kadokura <abbiscuit.com@gmail.com>
2019-06-27 21:10:34 +0900

    Add HOME page

--- src/components/RoboCard.js
+++ src/components/RoboCard.js
@@ -2,54 +2,291 @@ import React from 'react';
 
 const RoboCard = () => {
   return (
-    <div className="row">
-      <div className="col s12 m7">
-        <div className="card">
-          <div className="card-image">
-            <img src="https://materializecss.com/images/sample-1.jpg" />
-            <span className="card-title">Card Title</span>
-          </div>
-          <div className="card-content">
-            <p>
-              I am a very simple card. I am good at containing small bits of
-              information. I am convenient because I require little markup to
-              use effectively.
-            </p>
-          </div>
-          <div className="card-action">
-            <a href="#">This is a link</a>
-          </div>
-        </div>
-        <div className="card">
-          <div className="card-image">
-            <img src="https://materializecss.com/images/sample-1.jpg" />
-            <span className="card-title">Card Title</span>
-          </div>
-          <div className="card-content">
-            <p>
-              I am a very simple card. I am good at containing small bits of
-              information. I am convenient because I require little markup to
-              use effectively.
-            </p>
-          </div>
-          <div className="card-action">
-            <a href="#">This is a link</a>
+    <div className="container">
+      <div className="row">
+        <div className="col s12 m6 l4">
+          <div className="card">
+            <div className="card-image">
+              <img src="https://robohash.org/test?size=200x200" alt="" />
+            </div>
+            <div className="card-content">
+              <h3>John DOe</h3>
+              <p>Catch Phrase</p>
+            </div>
+            <div className="card-action">
+              <a
+                className="waves-effect waves-light btn  red accent-2"
+                href="#"
+              >
+                More...
+              </a>
+            </div>
           </div>
         </div>
-        <div className="card">
-          <div className="card-image">
-            <img src="https://materializecss.com/images/sample-1.jpg" />
-            <span className="card-title">Card Title</span>
+        <div className="col s12 m6 l4">
+          <div className="card">
+            <div className="card-image">
+              <img src="https://robohash.org/test?size=200x200" alt="" />
+            </div>
+            <div className="card-content">
+              <h3>John DOe</h3>
+              <p>Catch Phrase</p>
+            </div>
+            <div className="card-action">
+              <a
+                className="waves-effect waves-light btn  red accent-2"
+                href="#"
+              >
+                More...
+              </a>
+            </div>
           </div>
-          <div className="card-content">
-            <p>
-              I am a very simple card. I am good at containing small bits of
-              information. I am convenient because I require little markup to
-              use effectively.
-            </p>
+        </div>
+        <div className="col s12 m6 l4">
+          <div className="card">
+            <div className="card-image">
+              <img src="https://robohash.org/test?size=200x200" alt="" />
+            </div>
+            <div className="card-content">
+              <h3>John DOe</h3>
+              <p>Catch Phrase</p>
+            </div>
+            <div className="card-action">
+              <a
+                className="waves-effect waves-light btn  red accent-2"
+                href="#"
+              >
+                More...
+              </a>
+            </div>
           </div>
-          <div className="card-action">
-            <a href="#">This is a link</a>
+        </div>
+        <div className="col s12 m6 l4">
+          <div className="card">
+            <div className="card-image">
+              <img src="https://robohash.org/test?size=200x200" alt="" />
+            </div>
+            <div className="card-content">
+              <h3>John DOe</h3>
+              <p>Catch Phrase</p>
+            </div>
+            <div className="card-action">
+              <a
+                className="waves-effect waves-light btn  red accent-2"
+                href="#"
+              >
+                More...
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="col s12 m6 l4">
+          <div className="card">
+            <div className="card-image">
+              <img src="https://robohash.org/test?size=200x200" alt="" />
+            </div>
+            <div className="card-content">
+              <h3>John DOe</h3>
+              <p>Catch Phrase</p>
+            </div>
+            <div className="card-action">
+              <a
+                className="waves-effect waves-light btn  red accent-2"
+                href="#"
+              >
+                More...
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="col s12 m6 l4">
+          <div className="card">
+            <div className="card-image">
+              <img src="https://robohash.org/test?size=200x200" alt="" />
+            </div>
+            <div className="card-content">
+              <h3>John DOe</h3>
+              <p>Catch Phrase</p>
+            </div>
+            <div className="card-action">
+              <a
+                className="waves-effect waves-light btn  red accent-2"
+                href="#"
+              >
+                More...
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="col s12 m6 l4">
+          <div className="card">
+            <div className="card-image">
+              <img src="https://robohash.org/test?size=200x200" alt="" />
+            </div>
+            <div className="card-content">
+              <h3>John DOe</h3>
+              <p>Catch Phrase</p>
+            </div>
+            <div className="card-action">
+              <a
+                className="waves-effect waves-light btn  red accent-2"
+                href="#"
+              >
+                More...
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="col s12 m6 l4">
+          <div className="card">
+            <div className="card-image">
+              <img src="https://robohash.org/test?size=200x200" alt="" />
+            </div>
+            <div className="card-content">
+              <h3>John DOe</h3>
+              <p>Catch Phrase</p>
+            </div>
+            <div className="card-action">
+              <a
+                className="waves-effect waves-light btn  red accent-2"
+                href="#"
+              >
+                More...
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="col s12 m6 l4">
+          <div className="card">
+            <div className="card-image">
+              <img src="https://robohash.org/test?size=200x200" alt="" />
+            </div>
+            <div className="card-content">
+              <h3>John DOe</h3>
+              <p>Catch Phrase</p>
+            </div>
+            <div className="card-action">
+              <a
+                className="waves-effect waves-light btn  red accent-2"
+                href="#"
+              >
+                More...
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="col s12 m6 l4">
+          <div className="card">
+            <div className="card-image">
+              <img src="https://robohash.org/test?size=200x200" alt="" />
+            </div>
+            <div className="card-content">
+              <h3>John DOe</h3>
+              <p>Catch Phrase</p>
+            </div>
+            <div className="card-action">
+              <a
+                className="waves-effect waves-light btn  red accent-2"
+                href="#"
+              >
+                More...
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="col s12 m6 l4">
+          <div className="card">
+            <div className="card-image">
+              <img src="https://robohash.org/test?size=200x200" alt="" />
+            </div>
+            <div className="card-content">
+              <h3>John DOe</h3>
+              <p>Catch Phrase</p>
+            </div>
+            <div className="card-action">
+              <a
+                className="waves-effect waves-light btn  red accent-2"
+                href="#"
+              >
+                More...
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="col s12 m6 l4">
+          <div className="card">
+            <div className="card-image">
+              <img src="https://robohash.org/test?size=200x200" alt="" />
+            </div>
+            <div className="card-content">
+              <h3>John DOe</h3>
+              <p>Catch Phrase</p>
+            </div>
+            <div className="card-action">
+              <a
+                className="waves-effect waves-light btn  red accent-2"
+                href="#"
+              >
+                More...
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="col s12 m6 l4">
+          <div className="card">
+            <div className="card-image">
+              <img src="https://robohash.org/test?size=200x200" alt="" />
+            </div>
+            <div className="card-content">
+              <h3>John DOe</h3>
+              <p>Catch Phrase</p>
+            </div>
+            <div className="card-action">
+              <a
+                className="waves-effect waves-light btn  red accent-2"
+                href="#"
+              >
+                More...
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="col s12 m6 l4">
+          <div className="card">
+            <div className="card-image">
+              <img src="https://robohash.org/test?size=200x200" alt="" />
+            </div>
+            <div className="card-content">
+              <h3>John DOe</h3>
+              <p>Catch Phrase</p>
+            </div>
+            <div className="card-action">
+              <a
+                className="waves-effect waves-light btn  red accent-2"
+                href="#"
+              >
+                More...
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="col s12 m6 l4">
+          <div className="card">
+            <div className="card-image">
+              <img src="https://robohash.org/test?size=200x200" alt="" />
+            </div>
+            <div className="card-content">
+              <h3>John DOe</h3>
+              <p>Catch Phrase</p>
+            </div>
+            <div className="card-action">
+              <a
+                className="waves-effect waves-light btn  red accent-2"
+                href="#"
+              >
+                More...
+              </a>
+            </div>
           </div>
         </div>
       </div>
