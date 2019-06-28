b23cb7e49d2899216bc6b2fc09a62d4a077c39b4
Hiroshi Kadokura <abbiscuit.com@gmail.com>
2019-06-28 16:50:42 +0900

    Add router

---
+++ src/components/Robo.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Robo = ({ robot }) => {
+  return <div>{robot.id}</div>;
+};
+
+export default Robo;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
+import About from './components/About';
+import Navbar from './components/Navbar';
 
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { searchRobots, requestRobots } from './reducers';
 import { createLogger } from 'redux-logger';
 import thunkMiddleWare from 'redux-thunk';
+
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 const logger = createLogger();
 const rootReducer = combineReducers({ searchRobots, requestRobots });
@@ -19,9 +23,14 @@ const store = createStore(
 );
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <Router>
+    <Provider store={store}>
+      <Navbar />
+
+      <Route exact path="/" component={App} />
+      <Route path="/about" component={About} />
+    </Provider>
+  </Router>,
   document.getElementById('root')
 );
 
